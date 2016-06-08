a1f1b8c441afc94d1bd4ee7dd791b92007b2fec5
ojd2 <ojd2@st-andrews.ac.uk>
2016-06-08 14:09:36 +0100

    Updated file name and comments.

--- src/todo.elm
+++ src/todo.elm
@@ -1,1 +1,18 @@
-Hello
+Î{-
+A simple Todo List application for the following Dissertation Project for student 150033255.
+Project can be located on Github @ https://github.com/ojd2/FP_Elm_Todo for locating previous versions.
+
+The Application has been broken down into three parts using the popular MVC pattern:
+
+1. Model (Series of methods to capture the current state of the 'world' within the application)
+2. Update (Some methods to bind and move the current state of the 'world' within the application forward)
+3. View (Some methods that accept the passed current state of the 'world' and visualise the state through HTML)
+
+BUILD COMMANDS
+
+Run the following terminal commands from the root of the src folder:
+
+elm-make Todo.elm --output elm.js
+
+
+
