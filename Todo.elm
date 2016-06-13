df84e44814089201a262db8562aaf8c9e6998fb7
ojd2 <ojd2@st-andrews.ac.uk>
2016-06-13 16:50:44 +0100

    Updates all round.

--- Todo.elm
+++ Todo.elm
@@ -1,26 +1,30 @@
 {--
 
-1. ABOUT
+* ABOUT Application *
 
-A simple Todo List application for the following Dissertation Project for student 150033255.
+A simple Todo List Application for the following Dissertation Project for student 150033255.
 
-The project source code can be located on Github @ ... for locating previous versions.
+Also, the application has been created in order to explore and learn more about the Elm Language.
 
-2. ARCHITECTURE
+NOTE: See 'elm-package.json' for more information.
+
+* ARCHITECTURE *
 
 The Application has been broken down into three parts using the popular MVC pattern:
 
-- Model (Series of methods to capture the current state of the 'world' within the application)
-- Update (Some methods to bind and move the current state of the 'world' within the application forward)
-- View (Some methods that accept the passed current state of the 'world' and visualise the state through HTML)
+- Model (Series of methods to capture the current state of the 'world' within the application.)
+- Update (Some methods to bind and move the current state of the 'world' within the application forward.)
+- View (Some methods that accept the passed current state of the 'world' and visualise the state through HTML.)
+- Init (A model state declaration for the model to display on page load.)
+- Subscriptions (Any interactions that the model and program can not foresee, can be added and tracked using Subscriptions.)
 
-3. BUILD COMMANDS
+* BUILD COMMANDS *
 
 Run the following terminal commands from the root of the 'src' folder:
 
-'elm-make Todo.elm --output elm.js'
+'elm-make Todo2.elm --output Todo2.js'
 
-4. MODULES
+* MODULES *
 
 In Elm, Modules are exposed and integrated into the program using the 'exposing' method.
 
@@ -32,8 +36,11 @@ What is important to identify is that Elm uses two versions of importing modules
 * Qualified Imports will import a full range of helper functions.
 * UnQualified Imports will only import chosen helper functions to be used in different locations of the application state. 
 
---}
+5. CONTROLS
 
+(...)
+
+--}
 
 {--
 
@@ -46,160 +53,148 @@ With this in mind, we will be predominately using the following Modules:
 3. Html.Attributes (For adding Attributes and class types to created Html elements in the DOM tree)
 4. Html.Events (For adding Event handlers to the created Html elements in the DOM tree)
 5. Debug (For better Debugging information rendering to the compiler)
-6. Html.Lazy (Rather than immediately applying functions to their arguments, module will bundle up arguments up for later)
-7. Json.Decode (A package that will turn JSON strings and JS values into Elm values)
 8. String (A String package will be used for manipulating String Literal data types)
 
 --}
-import Html exposing (Html, h1, button, div, text, input, label)
-import Html.App as Todo
-import Html.Attributes exposing(class, style, placeholder, type', id, autofocus, value, for)
-import Html.Events exposing (onClick, onInput, on, keyCode, targetChecked, onCheck)
+
+import Html exposing (Html, div, text, h1, input, button, form)
+import Html.App
+import Html.Attributes exposing (class, id, value, placeholder, autofocus, type')
+import Html.Events exposing (onInput, onClick, onSubmit, on, keyCode)
 import Debug exposing (log)
-import Json.Decode as Json
 import String
+
 {--
 
-0.1 SET UP PROGRAM STRUCTURE
+1.0 SET UP MODEL COMPONENT
 
 We have to begin by setting up a program structure for our application. 
 Furthermore, we have to do this in order to apply the Elm architecture to our program and understand what it is we are 
 going to show in our browser. Therefore, we can follow the Elm Docs and have a program that follows the following outline:
 
-Main: =>
+main: =
   model = model
-  , view = view 
-  , update = update
+  {  init = init
+    , view = view 
+    , update = update
+    , subscriptions = subscriptions
+  }  
+
+Below, we use 0.17 prefixing and use `Html.App.program =' to assign the subsequent model logic.
+
+As we are not using any unique or special interactions (say some sort of server side interaction), we can declare subscriptions and init 
+as the currently are. 
 
 --}
+
+-- Set up Subscriptions
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- Set up Init
+init : (Model, Cmd Msg)
+init =
+  (initialModel, Cmd.none)
+
+-- Set up main program controls
 main =
-  Todo.beginnerProgram 
-  { model = model 
-  , view = view 
-  , update = update 
+  Html.App.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
   }
-{--
 
-0.2 SET UP Types
+type alias Todo =
+  { text: String
+  , id: Int
+  }
 
-....
+type alias Model =
+  { todos : List Todo
+  , textField : String
+  }
 
---}
-type alias Task = {
-  description: String
-  , uid: Int -- Unique Identifier.
-  , complete: Bool
-}
+initialModel : Model
+initialModel =
+  { todos = []
+  , textField = ""
+  }
 
-type alias Model = {
-  tasks: List Task
-  , text: String
-  , count: Int
-
-}
-
+-- Messages
 type Msg
-  = TaskDescription String
-  | EnterButton Int
-  | CompleteOrIncomplete Bool Int
-  | CreateTodo
+  = Add String
+  | Editing String
+  | Remove Int
+  | EnterButton String Int -- Seems like Elm does not have a onSubmit handler outside of <form>.
   | DeleteAll
-  --| DeleteSelected
-{--
 
-1.0 SET UP MODEL COMPONENT
-
-The state and structure of the application logic ::
-
-1. Model -> Task -> Model
-2. Model -> Model -> Model
-
---}
-model : Model
-model =
-  { text = "", tasks = [], count= 1 }
 {-- 
 
-1.1 SET UP KEYBOARD COMMANDS
-
-We assign a function 'onKeyUp' that sends a Elm 'msg' command. 
-
-It passes 'enter' which we will use to describe the enter button.
-
-This will eventually in the 'Update' section trigger the CreateTask function within the model state.
-
---}
-onKeyUp : (Int -> msg) -> Html.Attribute msg
-onKeyUp enter =
-  on "keyup" (Json.map enter keyCode)
-{-- 
-
-2.0 SET UP UPDATE COMPONENT
+1.0 SET UP UPDATE COMPONENT
 
 The 'Update' section of the MVC acts as a general dispatch for the models actions.
-
 Although, the update is not associated with the core components of an MVC pattern, in Elm and many other patterns, its role is crucial.
-
-It's crucial because it listens to all of the models interactions (also known in Elm as 'Signals' or 'Cmd' Actions)
-
-Actions take the current state of the model and yield a new model in turn, therefore the 'Update' is used for capturing new values from the Model.
-
-Once any new values are captured, our actions are dispatched and the models state is subsequently updated.
+It's crucial because it listens to all of the models interactions (also known in Elm as 'Signals' or 'Cmd' Actions).
+Messages (Msg or Actions in 0.16) take the current state of the model and yield a new model in turn, therefore the 'Update' is 
+used for capturing new values from the Model. Once any new values are captured, our actions are dispatched and the models state is 
+subsequently updated.
 
 --}
-update: Msg -> Model -> Model
-update action model = 
-  case action |> log "action" of
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Editing text ->
+      ( { model | textField = text } , Cmd.none)
 
-    TaskDescription inputText ->
-      { model 
-        | text = inputText 
-      }
+    Remove id ->
+    let 
+        newModel =
+          { model
+          | todos = List.filter (\m -> m.id /= id) model.todos
+          }
+        
+    in 
+        (newModel, Cmd.none)
 
-    EnterButton code ->
-      if code == 13 then -- We use the Int 13 because this represents the 'enter' keyboard number.
-        createTask model
-      else
-        model
+    Add description ->   
+    let
+        string = String.trim description
+        uId = List.map (\x -> x.id) model.todos 
+                |> List.maximum
+                |> Maybe.withDefault 0 
+        newTodo =
+            { text = string
+            , id = uId + 1
+            }
+        newModel =
+            { model
+            | todos = List.append model.todos [newTodo]
+            , textField = ""
+            }
+    in
+        (newModel, Cmd.none)
 
-    CompleteOrIncomplete checked uid ->
-      { model 
-        | tasks = List.map (\task -> updateTask task uid checked) model.tasks 
-      }
-
-    CreateTodo ->
-      createTask model
-
+    EnterButton description code ->
+    if code == 13
+        then update (Add description) model
+        else (model, Cmd.none)
+    
     DeleteAll ->
-      { model 
-      | tasks = []
-      , count = 1
-      }
+    let 
+        newModel =
+          { model
+          | todos = []
+          , textField = ""
+          }
+        
+    in 
+        (newModel, Cmd.none)
 
--- Our Update Declarations:
-createTask: Model -> Model
-createTask model =
-  if String.isEmpty model.text then
-    model
-  else
-    { model 
-        | count = model.count + 1 
-        , text = ""
-        , tasks = model.tasks ++ [ { description = model.text, uid = model.count, complete = False } ]
-      }
-
-updateTask: Task -> Int -> Bool -> Task
-updateTask task uid checked = 
-  if task.uid == uid then
-    {
-    task | complete = checked
-    }
-  else
-    task
-      
 {--
 
-3.0 SET UP VIEW COMPONENT
+2.0 SET UP VIEW COMPONENT
 
 What is the 'View'? 
 
@@ -220,50 +215,59 @@ How do we intend to visulaise in the DOM our state of the world (tasks) in HTML?
 Well, the 'View' component lets us do just that.
 
 --}
-view : Model -> Html Msg 
+
+-- View for the model state.
+view : Model -> Html Msg
 view model =
-  div []
-    [ -- Begin Parent 'div'.
-     div [id "header"] -- Begin Header inside Parent 'div'.
-      [ h1 [] [text "Enter a Todo!"] ]
-      , input -- Begin Input area for Todo Tasks inside Parent 'div'.
-        [ 
-        type' "text"
-          , placeholder "Add a task" 
-          , onInput TaskDescription, autofocus True
-          , value model.text
-          , onKeyUp EnterButton
-        ] [] -- End Input area for Todo Tasks inside Parent 'div'.
-        , div -- Begin Container for task lists inside Parent 'div'.
-        [id "tasks-holder"] (List.map taskHtml  (model.tasks))  
-        , div -- Begin Container for buttons inside Parent 'div'.
-        [class "button-group"] 
-        [
-         button -- Begin HTML for Buttons inside Button Container.
-         [ type' "button" , class "success addTask button", onClick CreateTodo] 
-         [ text "Add Task" ]
-         , button 
-         [type' "button", class "alert clearAll button", onClick DeleteAll] 
-         [ text "Clear All" ]
-         , button 
-         [type' "button", class "alert clearSelected button"] 
-         [ text "Clear Task" ]
-        ] -- End HTML for Buttons inside Button Container.
-    ] -- End Parent 'div'.
-taskHtml: Task -> Html Msg -- Begin View for each individual Task Todo to be displayed inside Main View of Model. 
-taskHtml task =
-  div [class "row"] [
-    div [class "small-12 columns"] [
-      input [ 
-        type' "checkbox"
-        , id ("checkbox_" ++ toString task.uid)
-        , onCheck (\bool -> CompleteOrIncomplete bool task.uid)
-      ]
-      [],
-      label [ 
-        for ("checkbox_" ++ toString task.uid), 
-        class (if task.complete then "completed" else "") -- If task is checked, then if complete is true, add the class "completed".
+  div [id "header"]
+    [ h1
+        []
+        [ text "Simple Todo Application:" ]
+    , form 
+        [ onSubmit (Add model.textField)
+        , id "tasks-holder"
         ] 
-      [text task.description]
+        [ input
+            [ value model.textField
+            , placeholder "Enter New Todo!"
+            , autofocus True
+            , onInput (\s -> Editing s)
+            , class "todo-input"
+            ]
+            []
+        , button
+            [ type' "submit"
+            , class "success addTask button"
+            ]
+            [text "Add Todo" ]
+            
+        , button
+            [ onClick (DeleteAll)
+            , class "alert clearAll button" ]
+            [text "Clear All" ]
+        ]
+    , div [ class "todo-list"] (todoList model)
     ]
-  ]
+
+-- View for the actual list container.
+todoList : Model -> List (Html Msg)
+todoList model =
+  case model.todos of
+    [] ->
+      [ div [] 
+        [ text "Please enter some tasks!" ] 
+      ]
+    todos ->
+      List.map todoItem todos
+
+-- View for a single Todo item.
+todoItem : Todo -> Html Msg
+todoItem todo =
+  div [id "todo-item"]
+    [ text todo.text
+    , button
+        [ onClick (Remove todo.id)
+        , class "remove-todo button tiny"
+        ]
+        [ text "Remove" ]
+    ]
